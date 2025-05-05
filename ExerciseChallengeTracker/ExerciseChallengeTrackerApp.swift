ba7913bc5f23f6a708a93c43b1c5f52b7c60a86a
b-the-coder <binhe1989@gmail.com>
2025-05-05 15:25:23 -0400

    Initial Commit

---
+++ ExerciseChallengeTracker/ExerciseChallengeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseChallengeTrackerApp.swift
+//  ExerciseChallengeTracker
+//
+//  Created by bin on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseChallengeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExerciseChallengeTrackerTests/ExerciseChallengeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseChallengeTrackerTests.swift
+//  ExerciseChallengeTrackerTests
+//
+//  Created by bin on 5/5/25.
+//
+
+import Testing
+@testable import ExerciseChallengeTracker
+
+struct ExerciseChallengeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
